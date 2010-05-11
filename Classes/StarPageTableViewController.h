b5735589e7bc4bf3e0bc46827ab69d060143db00
katax <katax@yahoo.co.jp>
2010-05-11 14:29:58 +0900

    modify

---
+++ Classes/StarPageTableViewController.h
@@ -0,0 +1,21 @@
+//
+//  StarPageTableViewController.h
+//  ContractForSE
+//
+//  Created by kata on 10/04/19.
+//  Copyright 2010 katax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ContentsViewController;
+
+@interface StarPageTableViewController : UITableViewController {
+	NSMutableArray *contentsArray;
+	ContentsViewController *contentsViewController; //deallocでの解放不要
+	NSInteger controllerSection;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil contentsViewContoroller:(ContentsViewController *)_contentsViewContoroller;
+
+@end
